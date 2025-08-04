3baf30de0e1d6a43b3547e383cfb404de4de6238
lehtztt <146241140+lehtztt@users.noreply.github.com>
2025-08-04 11:44:03 -0300

    .

---
+++ script.js
@@ -0,0 +1,12 @@
+const caixaPrincipal = document.querySelector(".caixa-pricipal");
+
+const caixaPerguntas = document.querySelector(".caixa-pricipal");
+
+const caixaAlternativas = document.querySelector(".caixa-pricipal");
+
+const caixaResultado = document.querySelector(".caixa-pricipal");
+
+const caixaResultado = document.querySelector(".caixa-pricipal");
+
+const lista = [ ];
+const perguntas [ ];
